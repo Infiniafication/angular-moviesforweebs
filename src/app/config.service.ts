cb04a435fafe43b22c3d66560e606feb66a2fa81
Infiniafication <isaachawks@gmail.com>
2020-01-31 01:28:07 +0800

    Minor Layout Changes

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -9,6 +9,34 @@ export interface Config {
   results: string[];
 }
 
+export interface MovieDetailsConfig {
+  page: number;
+  adult: string;
+  backdrop_path: string;
+  belongs_to_collection: string[];
+  budget: number;
+  genres: string[];
+  homepage: string;
+  id: number,
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: string;
+  poster_path: string;
+  production_companies: string[],
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: string;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
 @Injectable()
 export class ConfigService {
 
@@ -18,6 +46,9 @@ export class ConfigService {
 
   configUrl: string = 'https://api.themoviedb.org/3/discover/movie?api_key=5af1770915237b38ad8957ddff1b912f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=16&with_original_language=ja';
   
+  generateURL() {
+
+  }
 
   getConfig() {
     return this.http.get<Config>(this.configUrl);
