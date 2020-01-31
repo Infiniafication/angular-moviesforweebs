e1d9ded2880b1763307371b0077b281c0ac1d9bb
Infiniafication <isaachawks@gmail.com>
2020-01-31 20:27:47 +0800

    Implement @angular/material (mat-expansion-panel)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatExpansionModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
@@ -9,7 +11,7 @@ import { HottestComponent } from './hottest/hottest.component';
 import { ConfigService } from './config.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, HttpClientModule, MatExpansionModule ],
   declarations: [ AppComponent, BannerComponent, HottestComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ConfigService]
